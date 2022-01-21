162499f07bc0d58356c21ba263401f356ed91b38
Jananie Shree <72547884+JAANU-star@users.noreply.github.com>
2022-01-21 13:19:27 +0530

    Update Atm.java

--- Atm.java
+++ Atm.java
@@ -4,20 +4,20 @@ public class Atm {
 
     static Scanner sc = new Scanner(System.in);
     static int money[] = new int[4];
-    static String Admin_User = "Admin";
-    static String Admin_Password = "1234";
-    static int Admin_attempt = 1;
+    static String Ad_User = "Ad";
+    static String Ad_Password = "1234";
+    static int Ad_attempt = 1;
     static int State = 1;
     static Atm[] atm;
     static int Current_User = 0;
 
-    // Object creaction For User
+    
     public String User_Name, User_Pin;
     public int User_Balance = 0;
     public int User_attempt;
     public ArrayList<String> User_Statement;
 
-    // Constructor to initilize the objects
+   
     Atm(String Name, String Pin, int Balance) {
         this.User_Name = Name;
         this.User_Pin = Pin;
@@ -25,43 +25,43 @@ public class Atm {
         this.User_attempt = 1;
     }
 
-    // Admin LOgin
-    public static void Admin_Login() {
-        while (Admin_attempt <= 3) {
+    
+    public static void Ad_Login() {
+        while (Ad_attempt <= 3) {
             System.out.print("\033[H\033[2J");
-            System.out.print("\t Welcome Admin \nEnter the Admin User Id : ");
+            System.out.print("\t Welcome Ad \nEnter the Ad User Id : ");
             String User_Id = sc.next();
             sc.nextLine();
-            System.out.print("\nEnter the Admin Password : ");
+            System.out.print("\nEnter the Ad Password : ");
             String User_Password = sc.next();
             sc.nextLine();
 
-            // Checks the user id and Password
-            if (Admin_User.equals(User_Id) && Admin_Password.equals(User_Password)) {
-                Admin();
+            
+            if (Ad_User.equals(User_Id) && Ad_Password.equals(User_Password)) {
+                Ad();
             } else {
-                Admin_attempt += 1;
+                Ad_attempt += 1;
             }
         }
 
-        // If the attemp is more than 3 the account has been locked :-)
-        if (Admin_attempt == 4) {
+        
+        if (Ad_attempt == 4) {
             System.out.println("Your Account Has been Locked  :-)");
             main(null);
         }
     }
 
-    // Admin panel
-    public static void Admin() {
+    
+    public static void Ad() {
         int i = 1;
         System.out.print("\033[H\033[2J");
         while (i != 0) {
             System.out.println(
-                    "\t Welcome Admin Panel \n 1 => Deposit Money in ATM\n 2 => Check Balance in ATM\n 3 => Back");
+                    "\t Welcome Ad Panel \n 1 => Deposit Money in ATM\n 2 => Check Balance in ATM\n 3 => Back");
             int option = sc.nextInt();
             switch (option) {
 
-                // Deposit MOney
+                
                 case 1:
                     System.out.print("Enter 2000 count : ");
                     money[0] += sc.nextInt() * 2000;
@@ -77,7 +77,7 @@ public class Atm {
                     System.out.println("\033[H\033[2J");
                     break;
 
-                // Check Balance
+                
                 case 2:
                     System.out.println("2000 count : " + (money[0] / 2000) + " Amount Present : " + money[0]);
                     System.out.println("500 count : " + (money[1] / 500) + " Amount Present : " + money[1]);
@@ -86,21 +86,21 @@ public class Atm {
                     System.out.println("Total Amount Present : " + (money[0] + money[1] + money[2] + money[3]));
                     break;
 
-                // Exit to the main menu
+                
                 case 3:
                     main(null);
                     i = 0;
                     break;
 
-                // None of the input matches
+                
                 default:
                     System.out.println("Enter the valid case !");
             }
         }
     }
 
-    // User Auth
-    public static void User_Login() {
+    
+    public static void U_Log() {
         int k = 1;
         while (atm[Current_User].User_attempt <= 3 && k != 0) {
             System.out.print("\033[H\033[2J");
@@ -111,10 +111,9 @@ public class Atm {
             String User_Password = sc.next();
             sc.nextLine();
 
-            // Checks the user id and Password
+            
             for (int i = 0; i < 2; i++) {
-                // System.out.println("-------");
-                // System.out.println(atm[i].User_Name);
+                
                 if (atm[i].User_Name.equals(User_Id) && atm[i].User_Pin.equals(User_Password)) {
                     Current_User = i;
                     User();
@@ -125,14 +124,14 @@ public class Atm {
             atm[Current_User].User_attempt += 1;
         }
 
-        // If the attemp is more than 3 the account has been locked :-)
+        
         if (atm[Current_User].User_attempt == 4) {
             System.out.println("Your Account Has been Locked  :-)");
             main(null);
         }
     }
 
-    // User Panel
+    
     public static void User() {
         int i = 1;
         while (i != 0) {
@@ -141,7 +140,7 @@ public class Atm {
             int option = sc.nextInt();
             switch (option) {
 
-                // Deposit money
+                
                 case 1:
                     System.out.print("Enter 2000 count : ");
                     int x = sc.nextInt() * 2000;
@@ -167,20 +166,20 @@ public class Atm {
                     System.out.println("\033[H\033[2J");
                     break;
 
-                // Check Balance
+                
                 case 2:
                     System.out.println("User Name : " + atm[Current_User].User_Name);
                     System.out.println("User Balance :" + atm[Current_User].User_Balance);
                     break;
 
-                // Widthdraw Money
+                
                 case 3:
                     System.out.print("\033[H\033[2J");
                     System.out.print("Enter the Amount to be WidthDrawn : ");
                     int Withdraw_Amount = sc.nextInt();
                     if (Withdraw_Amount <= (money[0] + money[1] + money[2] + money[3])) {
                         if (Withdraw_Amount <= atm[Current_User].User_Balance) {
-                            Width_draw(Withdraw_Amount);
+                            Width_draw_Amount(Withdraw_Amount);
                         } else {
                             System.out.println("Insufficient Amount In your Account !");
                         }
@@ -190,7 +189,7 @@ public class Atm {
                     }
                     break;
 
-                // Mini Statement
+                
                 case 4:
                     System.out.println("Mini Statement !");
                     for (int k = atm[Current_User].User_Statement.size() - 1; k >= 0; k--) {
@@ -198,7 +197,7 @@ public class Atm {
                     }
                     break;
 
-                // Change Pasword
+                
                 case 5:
                     System.out.print("Enter the New Password : ");
                     String New_Pin = sc.next();
@@ -207,19 +206,19 @@ public class Atm {
                     System.out.println("Pin has been Changed !");
                     break;
 
-                // Exit to the main menu
+                
                 case 6:
                     main(null);
                     break;
 
-                // Default Statement
+                
                 default:
                     System.out.println("Enter the valid case !");
             }
         }
     }
 
-    public static void Width_draw(int Widthdraw_Amount) {
+    public static void Width_draw_Amount(int Widthdraw_Amount) {
         int temp = Widthdraw_Amount;
         int presentCount1[] = { money[0] / 2000, money[1] / 500, money[2] / 200, money[3] / 100 };
         int presentCount[] = new int[4];
@@ -263,7 +262,7 @@ public class Atm {
 
         int i = 1;
 
-        // Object creation
+        
 
         if (State == 1) {
             atm = new Atm[2];
@@ -274,23 +273,23 @@ public class Atm {
             State = 0;
         }
 
-        // Choosing the Type Of user Based on the User's Chooise !
+        
         while (i != 0) {
-            System.out.println("Welcome to ATM \n 1 => Admin \n 2 => User \n 3 => Exit");
+            System.out.println("Welcome to ATM \n 1 => Ad \n 2 => User \n 3 => Exit");
             int userType = sc.nextInt();
 
-            // Go to admin panel
+            
             if (userType == 1) {
-                Admin_Login();
+                Ad_Login();
                 break;
             }
-            // Go to user Pannel
+            
             else if (userType == 2) {
-                User_Login();
+                U_Log();
                 break;
             }
 
-            // Exit from the main menu
+            
             else if (userType == 3) {
                 i = 0;
                 System.out.println("Thank You for Visiting US !");
@@ -298,7 +297,7 @@ public class Atm {
                 break;
             }
 
-            // None of the cases matches
+            
             else {
                 System.out.print("\033[H\033[2J");
                 System.out.println("Please Enter the valid Case !");
